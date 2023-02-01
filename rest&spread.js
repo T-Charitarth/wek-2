1a81bb4996a413e63d62dae17e6d6702fc08e48c
T Charitarth <20b81a0568@cvr.ac.in>
2023-02-01 10:38:27 +0530

    week-2 work started

---
+++ rest&spread.js
@@ -0,0 +1,11 @@
+function sum(name , ...args)
+{
+    console.log("Name is : ", name);
+    let res=0;
+    for(let i in args)
+    {
+        res+=args[i];
+        return res;
+    }
+}
+console.log(sum("CVR",10,20,30));
